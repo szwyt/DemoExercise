b39613b79808958d9bf91a876da8c1ff5c13a3f2
yfbaifan <1214720854@qq.com>
2021-06-30 12:48:52 +0800

    1

--- DockerPublish/publish.sh
+++ DockerPublish/publish.sh
@@ -1,10 +1,11 @@
 sed -i 's/\r$//' publish.sh
-docker rmi $(docker images | grep "none" | awk '{print $3}')
-docker rm -f mongodb_demo
-echo "Container deletion successful"
+docker ps -a | grep -w mongodb_demo | awk '{print $1 }'|xargs docker rm -f
+echo "Container delete successful"
 docker rmi -f mongodb
-echo "Image deletion successful"
+echo "Image delete successful"
 docker build  -t  mongodb -f mongodb ../
 echo "image create successful"
 docker run -p 5001:5001 --name mongodb_demo --restart=always -d  mongodb
 echo "publish successful"
+docker rmi $(docker images | grep "none" | awk '{print $3}')
+echo "Image is none delete successful"
