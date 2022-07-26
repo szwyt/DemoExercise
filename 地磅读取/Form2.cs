879ae6e654632e5734f1d8efed8b7aaafb2cdefb
szw <1214720854@qq.com>
2022-07-27 06:12:51 +0800

    1.

--- 地磅读取/Form2.cs
+++ 地磅读取/Form2.cs
@@ -25,98 +25,95 @@ namespace 地磅读取
         {
             thread = new Thread(new ThreadStart(async () =>
             {
-                await Task.Run(async () =>
+                string chromePath = Path.Combine(AppContext.BaseDirectory, ".local-chromium", "Win64-970485", "chrome-win");
+                // 如果不存在chrome就下载一个
+                if (!Directory.Exists(chromePath))
                 {
-                    string chromePath = Path.Combine(AppContext.BaseDirectory, ".local-chromium", "Win64-970485", "chrome-win");
-                    // 如果不存在chrome就下载一个
-                    if (!Directory.Exists(chromePath))
+                    using (var browserFetcher = new BrowserFetcher())
                     {
-                        using (var browserFetcher = new BrowserFetcher())
+                        await browserFetcher.DownloadAsync();
+                    };
+
+                }
+
+                var list = File.ReadAllLines($"{Path.Combine(AppContext.BaseDirectory, "siteurl.txt")}");
+                int success = 1;
+                int error = 1;
+                int noimage = 1;
+                for (int i = 0; i < list.Count(); i++)
+                {
+                    int j = i + 1;
+                    Console.WriteLine($"第{j}条数据------->{DateTime.Now}");
+                    try
+                    {
+                        var launch = new LaunchOptions
                         {
-                            await browserFetcher.DownloadAsync();
+                            Headless = true,
+                            ExecutablePath = Path.Combine(chromePath, "chrome.exe")
                         };
-
-                    }
-
-                    var list = File.ReadAllLines($"{Path.Combine(AppContext.BaseDirectory, "siteurl.txt")}");
-                    int success = 1;
-                    int error = 1;
-                    int noimage = 1;
-                    for (int i = 0; i < list.Count(); i++)
-                    {
-                        int j = i + 1;
-                        Console.WriteLine($"第{j}条数据------->{DateTime.Now}");
-                        try
+                        using (var browser = await Puppeteer.LaunchAsync(launch))
                         {
-                            var launch = new LaunchOptions
+                            using (var page = await browser.NewPageAsync())
                             {
-                                Headless = true,
-                                ExecutablePath = Path.Combine(chromePath, "chrome.exe")
-                            };
-                            using (var browser = await Puppeteer.LaunchAsync(launch))
-                            {
-                                using (var page = await browser.NewPageAsync())
+                                await page.SetViewportAsync(new ViewPortOptions
                                 {
-                                    await page.SetViewportAsync(new ViewPortOptions
+                                    Width = 1920,
+                                    Height = 1080,
+                                });
+                                var item = list[i];
+                                var result = await page.GoToAsync($"{item}");
+                                await page.WaitForTimeoutAsync(1500);
+
+                                if (result != null && result.Status == System.Net.HttpStatusCode.OK)
+                                {
+                                    string fileName = $"Files/{j}.Png";
+                                    string outputFile = $"{AppContext.BaseDirectory}/{fileName}";
+                                    await page.ScreenshotAsync($"{outputFile}", new ScreenshotOptions()
                                     {
-                                        Width = 1920,
-                                        Height = 1080,
+                                        Type = ScreenshotType.Png,
+                                        FullPage = true,
                                     });
-                                    var item = list[i];
-                                    var result = await page.GoToAsync($"{item}");
-                                    await page.WaitForTimeoutAsync(1500);
+                                    Console.WriteLine($"{j}------->{DateTime.Now}------->{outputFile}------->成功数：{success++}");
 
-                                    if (result != null && result.Status == System.Net.HttpStatusCode.OK)
-                                    {
-                                        string fileName = $"Files/{j}.Png";
-                                        string outputFile = $"{AppContext.BaseDirectory}/{fileName}";
-                                        await page.ScreenshotAsync($"{outputFile}", new ScreenshotOptions()
-                                        {
-                                            Type = ScreenshotType.Png,
-                                            FullPage = true,
-                                        });
-                                        Console.WriteLine($"{j}------->{DateTime.Now}------->{outputFile}------->成功数：{success++}");
+                                    ////第2种
+                                    //using (var stream = await page.ScreenshotStreamAsync(new ScreenshotOptions { FullPage = false }))
+                                    //{
 
-                                        ////第2种
-                                        //using (var stream = await page.ScreenshotStreamAsync(new ScreenshotOptions { FullPage = false }))
-                                        //{
+                                    //    byte[] srcBuf = new Byte[stream.Length];
+                                    //    stream.Read(srcBuf, 0, srcBuf.Length);
+                                    //    stream.Seek(0, SeekOrigin.Begin);
+                                    //    using (FileStream fs = new FileStream($"{outputFile}", FileMode.Create, FileAccess.Write))
+                                    //    {
+                                    //        fs.Write(srcBuf, 0, srcBuf.Length);
+                                    //    }
+                                    //}
 
-                                        //    byte[] srcBuf = new Byte[stream.Length];
-                                        //    stream.Read(srcBuf, 0, srcBuf.Length);
-                                        //    stream.Seek(0, SeekOrigin.Begin);
-                                        //    using (FileStream fs = new FileStream($"{outputFile}", FileMode.Create, FileAccess.Write))
-                                        //    {
-                                        //        fs.Write(srcBuf, 0, srcBuf.Length);
-                                        //    }
-                                        //}
-
-                                        //第3种
-                                        //var buffer = await page.ScreenshotDataAsync(new ScreenshotOptions { Type = ScreenshotType.Png, FullPage = true });
-                                        //if (buffer.Length < 20 * 1024)
-                                        //{
-                                        //    using (FileStream fs = new FileStream($"{outputFile}", FileMode.Create, FileAccess.Write))
-                                        //    {
-                                        //        fs.Write(buffer, 0, buffer.Length);
-                                        //    }
-                                        //    Console.WriteLine($"{j}----------------->{DateTime.Now}----------------->" + outputFile);
-                                        //}
-                                        //else
-                                        //    Console.WriteLine($"{j}----------------->{DateTime.Now}----------------->" + "buffer is big data");
-                                    }
-                                    else
-                                    {
-                                        Console.WriteLine($"{j}------->{DateTime.Now}------->no generate image------->noimages数：{noimage++}");
-                                    }
+                                    //第3种
+                                    //var buffer = await page.ScreenshotDataAsync(new ScreenshotOptions { Type = ScreenshotType.Png, FullPage = true });
+                                    //if (buffer.Length < 20 * 1024)
+                                    //{
+                                    //    using (FileStream fs = new FileStream($"{outputFile}", FileMode.Create, FileAccess.Write))
+                                    //    {
+                                    //        fs.Write(buffer, 0, buffer.Length);
+                                    //    }
+                                    //    Console.WriteLine($"{j}----------------->{DateTime.Now}----------------->" + outputFile);
+                                    //}
+                                    //else
+                                    //    Console.WriteLine($"{j}----------------->{DateTime.Now}----------------->" + "buffer is big data");
+                                }
+                                else
+                                {
+                                    Console.WriteLine($"{j}------->{DateTime.Now}------->no generate image------->noimages数：{noimage++}");
                                 }
                             }
                         }
-                        catch (Exception ex)
-                        {
-                            var k = i + 1;
-                            Console.WriteLine($"{k}------->{DateTime.Now}------->error:{ex.Message}------->异常数：{error++}");
-                        }
                     }
-                });
+                    catch (Exception ex)
+                    {
+                        var k = i + 1;
+                        Console.WriteLine($"{k}------->{DateTime.Now}------->error:{ex.Message}------->异常数：{error++}");
+                    }
+                }
             }));
             thread.IsBackground = true;
             thread.Start();
